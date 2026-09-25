492de10d0ec2d2b643235759bbce758b40250984
CardenasDev <132156793+CardenasDev@users.noreply.github.com>
2026-09-25 15:18:39 -0500

    1.0.14
    Ajuste enviroment

--- Front/src/app/core/config/app-version.ts
+++ Front/src/app/core/config/app-version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = '1.0.13' as const;
+export const APP_VERSION = '1.0.14' as const;
 export const APP_VERSION_LABEL = `v${APP_VERSION}` as const;
---
+++ Front/src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://cardenas.com.co/Demos/SA/Back/public/index.php/api',
+  backendUrl: 'https://cardenas.com.co/Demos/SA/Back/public',
+};
